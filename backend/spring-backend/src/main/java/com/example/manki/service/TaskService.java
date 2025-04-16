8070a353a9caa2006a150c70f1fb41e9cff66ace
Bharatdwaj3 <badrikedar.jaydutt.456@gmail.com>
2025-04-16 17:32:56 +0530

    draft-spring_react-1

---
+++ backend/spring-backend/src/main/java/com/example/manki/service/TaskService.java
@@ -0,0 +1,38 @@
+package com.example.manki.service;
+
+import org.springframework.stereotype.Service;
+import com.example.manki.model.Task;
+import com.example.manki.repository.TaskRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class TaskService {
+    private final TaskRepository taskRepository;
+    public TaskService(TaskRepository taskRepository){
+        this.taskRepository=taskRepository;
+    }
+
+    public List<Task> getAllTasks(){
+        return taskRepository.findAll();
+    }
+
+    public Optional<Task> getTaskById(Long id){
+        return taskRepository.findById(id);
+    }
+
+    public Task addTask(Task task){
+        return taskRepository.save(task);
+    }
+
+    public Optional<Task> updateTaskStatus(Long id, boolean completed){
+        return taskRepository.findById(id).map(task->{
+            return taskRepository.save(task);
+        });
+    }
+
+    public void deleteTask(Long id){
+        taskRepository.deleteById(id);
+    }
+}
